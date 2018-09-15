b3d44414f76d8078a93408cf5b0dabde0edb4dbf
Juanquita <juanca.davalos.t@gmail.com>
2018-09-15 11:31:58 -0400

    Finalizado compras

--- src/tiendaVideos/Compra.java
+++ src/tiendaVideos/Compra.java
@@ -1,5 +1,35 @@
 package tiendaVideos;
+import java.sql.SQLException;
 
 public class Compra {
-
+	int codproducto;
+	int cantidad;
+	int precio;
+	String fecha;
+	// nos falta crear la clase consultas para que nos deje de aparecer el error  mostrado en el codigo 
+    consultas consulta = new consultas();
+	
+	public Compra(int codproducto,int cantidad,int precio,String fecha){
+        this.codproducto=codproducto;
+        this.cantidad=cantidad;
+        this.precio=precio;
+        this.fecha=fecha;
+	}
+	public int getproducto(){
+		return codproducto;
+	}
+	public int getpcantidad(){
+		return cantidad;
+	}
+	public int getprecio(){
+		return precio;
+	}
+	public String fecha(){	
+		return fecha;
+	}
+	public void comprar(MySQL mySQL, Compra compra) throws SQLException {
+	// nos falta crear la clase consultas para que nos deje de aparecer el error  mostrado en el codigo
+		consulta.comprar(mySQL,compra);	
+	}
 }
+
